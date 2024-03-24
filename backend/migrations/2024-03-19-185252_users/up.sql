a812281e7f2fa27975cd931e9d1c6fff871cde91
Joseph Murgor <joskip5@gmail.com>
2024-03-24 22:17:36 +0300

    migrations file for users table

--- backend/migrations/2024-03-19-185252_users/up.sql
+++ backend/migrations/2024-03-19-185252_users/up.sql
@@ -1,14 +1,15 @@
 CREATE TABLE users (
                        id serial not null primary key,
                        email VARCHAR(255) NOT NULL,
+                       name VARCHAR(255) NOT NULL,
                        password VARCHAR(255) NOT NULL,
                        slug VARCHAR(255) NOT NULL,
-                       balance DECIMAL(255)
+                       balance VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (email, password, slug, balance)
+INSERT INTO users (email, name,password, slug, balance)
 VALUES
-    ('user1@example.com', 'password123', 'user1_slug', 100.00),
-    ('user2@example.com', 'pass456', 'user2_slug', NULL),
-    ('user3@example.com', 'securepass', 'user3_slug', 50.75),
-    ('user4@example.com', '12345pass', 'user4_slug', 200.50);
+    ('user1@example.com','user1', 'password123', 'user1_slug', 100.00),
+    ('user2@example.com','user2', 'pass456', 'user2_slug', 20),
+    ('user3@example.com', 'user3','securepass', 'user3_slug', 50.75),
+    ('user4@example.com', 'user4','12345pass', 'user4_slug', 200.50);

44d4e6e08198185d810805e32e0aedf37203ea50
Joseph Murgor <joskip5@gmail.com>
2024-04-17 15:04:08 +0300

    remove duplication and use lnd connect function

--- backend/src/invoice.rs
+++ backend/src/invoice.rs
@@ -65,21 +65,8 @@ pub async  fn create_invoice(amount: i32){
 
 }
 pub async  fn invoice_look_up(payment_addr: Vec<u8>)-> InvoiceStatus{
-
-    //let payment_address= payment_addr.as_bytes().to_vec();
-    let address="https://127.0.0.1:10001";
-    //let cert_file_path ="/Users/jose/.polar/networks/1/volumes/lnd/alice/tls.cert";
-
-    let cert_file_path="/Users/jose/.polar/networks/2/volumes/lnd/alice/tls.cert";
-    let macaroon_file_path="/Users/jose/.polar/networks/2/volumes/lnd/alice/data/chain/bitcoin/regtest/admin.macaroon";
-
-    let cert_path = cert_file_path.to_string();
-    let macaroon_path = macaroon_file_path.to_string();
-    let address_path= address.to_string();
-    // Connecting to LND requires only address, cert file, and macaroon file
-    let mut client = tonic_lnd::connect(address_path, cert_path, macaroon_path)
-        .await
-        .expect("failed to connect");
+    //connect to the node 
+    let client = connect();
 
     let invoice_lookup=client.invoices()
         .lookup_invoice_v2(LookupInvoiceMsg{ lookup_modifier: 1, invoice_ref: Option::from(InvoiceRef::PaymentAddr(payment_addr)) })
